4941550264b795cf8afaff1635de8b8268a580c5
Ariel Zhen <zhenpingping@live.com>
2018-07-23 23:11:24 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -31,10 +31,10 @@ class InteractiveRecord
   end
 
   def table_name_for_insert
-    self.class.table_name 
-  end 
+    self.class.table_name
+  end
 
-  def col_name_for_insert 
+  def col_name_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
-  end 
+  end
 end
